6809e24d042b1f9d5d1cfbb3d12d12510547db71
Aleksei <grigorev@hit.edu.cn>
2018-09-09 19:40:42 +1000

    tag's info

---
+++ scripts/data_stats.py
@@ -0,0 +1,54 @@
+#import system stuff
+import sys
+import argparse
+import os.path as osp
+import numpy as np
+
+tags = ['dog', 'cat', 'mouse', 'rabbit', 'bird', 'scenery', 'local customs',
+'dessing', 'baby', 'selfie - male', 'selfie - female', 'dessert making',
+'seafood making', 'streetside snacks', 'drinks', 'hot pot', 'claw crane',
+'handsign dance', 'street dance', 'international dance', 'pole dance', 
+'ballet', 'square dancing', 'folk dance', 'drawing', 'handwriting', 'latter art',
+'sand drawing', 'slime', 'origami', 'knitting', 'hair accessory', 'pottery',
+'phone case', 'drums', 'guitar', 'piano', 'guzheng', 'violin', 'cello',
+'hulusi', 'singing', 'games', 'entertainment', 'animation' , 'word art voicing',
+'yoga', 'fitness', 'skateboard', 'basketball', 'parkour', 'diving', 'billiards',
+'football', 'badminton', 'table tennis', 'brow painting', 'eyeliner', 'skincare',
+'lipgloss', 'makeup removal', 'nail cosmetic', 'hair cosmetic']
+
+def main(args):
+    files = 0
+    multi_label = 0 
+    working_dir = osp.dirname(osp.abspath(__file__))
+    labels = np.zeros(63)
+    with open(args.ann_file) as infile, open(osp.join(working_dir, "stats.txt"), "w") as outfile:
+        for line in infile:
+            sp_line = line.split(",")
+            #drop files that doesnt exists
+            filepath = osp.join(args.train_dir, sp_line[0])
+            if not osp.exists(filepath):
+                continue
+
+            if len(sp_line) > 2:
+                multi_label += 1
+            for el in sp_line:
+                try:
+                     labels[int(el)] += 1
+                except (ValueError, TypeError):
+                    files += 1
+        
+        print("labels")
+        print(labels)
+        for i in range(len(labels)):
+            #print("label: {} | tag: {} | files: {}".format(i, tags[i], labels[i]))
+            print("| {} | {} | {} |".format(i, tags[i], labels[i]))
+            outfile.write("label: {} | tag: {} | files: {} \n".format(i, tags[i], labels[i]))
+        outfile.write("amount of files: {}, files with more than 1 label: {} ".format(files, multi_label))
+        #outfile.flush() 
+        #outfile.close()
+if __name__ == '__main__':
+    #parameters?
+    parser = argparse.ArgumentParser(description="collect the labels statistic ")
+    parser.add_argument('--ann_file', type=str, metavar='PATH', help = "path to the annotation file")
+    parser.add_argument('--train_dir', type=str, metavar='PATH', help = "path to the train folder")
+    main(parser.parse_args())
---
+++ stats.md
@@ -0,0 +1,65 @@
+| label       | tag       | files  |
+| ------------- |:-------------:| -----:|
+| 0 | dog | 9556.0 |
+| 1 | cat | 9242.0 |
+| 2 | mouse | 624.0 |
+| 3 | rabbit | 760.0 |
+| 4 | bird | 890.0 |
+| 5 | scenery | 9215.0 |
+| 6 | local customs | 482.0 |
+| 7 | dessing | 2167.0 |
+| 8 | baby | 11203.0 |
+| 9 | selfie - male | 1689.0 |
+| 10 | selfie - female | 17339.0 |
+| 11 | dessert making | 119.0 |
+| 12 | seafood making | 313.0 |
+| 13 | streetside snacks | 581.0 |
+| 14 | drinks | 613.0 |
+| 15 | hot pot | 239.0 |
+| 16 | claw crane | 729.0 |
+| 17 | handsign dance | 8290.0 |
+| 18 | street dance | 9762.0 |
+| 19 | international dance | 5607.0 |
+| 20 | pole dance | 823.0 |
+| 21 | ballet | 285.0 |
+| 22 | square dancing | 409.0 |
+| 23 | folk dance | 455.0 |
+| 24 | drawing | 106.0 |
+| 25 | handwriting | 16.0 |
+| 26 | latter art | 395.0 |
+| 27 | sand drawing | 373.0 |
+| 28 | slime | 8507.0 |
+| 29 | origami | 291.0 |
+| 30 | knitting | 322.0 |
+| 31 | hair accessory | 484.0 |
+| 32 | pottery | 332.0 |
+| 33 | phone case | 454.0 |
+| 34 | drums | 989.0 |
+| 35 | guitar | 2868.0 |
+| 36 | piano | 5175.0 |
+| 37 | guzheng | 2967.0 |
+| 38 | violin | 327.0 |
+| 39 | cello | 501.0 |
+| 40 | hulusi | 488.0 |
+| 41 | singing | 392.0 |
+| 42 | games | 2067.0 |
+| 43 | entertainment | 293.0 |
+| 44 | animation | 98.0 |
+| 45 | word art voicing | 507.0 |
+| 46 | yoga | 922.0 |
+| 47 | fitness | 881.0 |
+| 48 | skateboard | 822.0 |
+| 49 | basketball | 319.0 |
+| 50 | parkour | 248.0 |
+| 51 | diving | 282.0 |
+| 52 | billiards | 426.0 |
+| 53 | football | 466.0 |
+| 54 | badminton | 449.0 |
+| 55 | table tennis | 368.0 |
+| 56 | brow painting | 456.0 |
+| 57 | eyeliner | 384.0 |
+| 58 | skincare | 517.0 |
+| 59 | lipgloss | 431.0 |
+| 60 | makeup removal | 491.0 |
+| 61 | nail cosmetic | 344.0 |
+| 62 | hair cosmetic | 790.0 |
