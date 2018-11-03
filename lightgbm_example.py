a67d8ad6f4d2fb77ab1b97d43fa309c6041f5010
Aleksei <grigorev@hit.edu.cn>
2018-11-03 17:01:43 +0800

    add features aggregation

--- lightgbm_example.py
+++ lightgbm_example.py
@@ -12,26 +12,34 @@ from sklearn.decomposition import PCA
 import lightgbm as lgb
 
 from sklearn.metrics import accuracy_score
-
+#sorted(glob.glob('5_frames_val/torch_tags_*.th'), key = os.path.getmtime)
 def main(args):
     #load train features
-    train_features = [torch.load(c) for c in glob.glob(osp.join(args.train_dir, "*.th"))]
+    train_features = [torch.load(c) for c in sorted(glob.glob(osp.join(args.train_dir, "torch_features_*.th")), key = os.path.getmtime)]
     train_features = torch.cat((train_features), dim = 0)
+    train_tags = [torch.load(c) for c in sorted(glob.glob(osp.join(args.train_dir, "torch_tags_*.th")), key = os.path.getmtime)]
+    train_tags = torch.cat((train_tags), dim = 0)
 
     #load validation features
-    val_features = [torch.load(c) for c in glob.glob(osp.join(args.val_dir, "*.th"))]
+    val_features = [torch.load(c) for c in sorted(glob.glob(osp.join(args.val_dir, "torch_features_*.th")), key = os.path.getmtime)]
     val_features = torch.cat((val_features), dim = 0)
+    val_tags = [torch.load(c) for c in sorted(glob.glob(osp.join(args.val_dir, "torch_tags_*.th")), key = os.path.getmtime)]
+    val_tags = torch.cat((val_tags), dim = 0)
+
+    train_features = torch.mean(train_features, 1)
+    val_features = torch.mean(val_features, 1)
+
     print ('train features: {}'.format(train_features.size()))
     print ('val features: {}'.format(val_features.size()))
 
     print(train_features.size())
 
-    X_train = train_features[:, 1:].numpy()
-    y_train = train_features[:, 0].numpy().astype(int)
+    X_train = train_features.numpy()
+    y_train = train_tags.numpy().astype(int)
     #print(train_features)
 
-    X_val = val_features[:, 1:].numpy()
-    y_val = val_features[:, 0].numpy().astype(int)
+    X_val = val_features..numpy()
+    y_val = val_tags.numpy().astype(int)
 
     n_classes = len(np.unique(y_train))
 
@@ -74,6 +82,7 @@ def main(args):
     y_pred = gbm.predict(X_val, num_iteration=gbm.best_iteration)
 
     print('accuracy score: {}'.format(accuracy_score(y_val, np.argmax(y_pred, axis = 1))))
+    
 
 
 if __name__ == '__main__':
